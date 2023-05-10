3787622567432e658d043dd7dcf048998f73c8eb
Arush Sharma <arush24astar@gmail.com>
2023-05-10 13:53:59 +0530

    change author and desc in config and update gitignore

--- nativescript.config.ts
+++ nativescript.config.ts
@@ -1,11 +1,18 @@
 import { NativeScriptConfig } from '@nativescript/core';
 
 export default {
-  id: 'org.nativescript.quickpdf',
+  id: 'org.arushsharma24.quickpdf',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
     v8Flags: '--expose_gc',
     markingMode: 'none'
-  }
+  },
+  title: 'Quick PDF',
+  version: '0.1.0',
+  author: {
+    name: 'Arush Sharma',
+    email: 'arush24astar@gmail.com'
+  },
+  description: 'A quick PDF opener'
 } as NativeScriptConfig;
