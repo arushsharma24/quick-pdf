4297a70170c326a73be47510652d4a2696c457a9
Arush Sharma <arush24astar@gmail.com>
2023-05-31 23:34:59 +0530

    finally it shows the pdf atleast yessssss, first time this has worked !!!! T_T

--- App.tsx
+++ App.tsx
@@ -1,11 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
+import React, { Component } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -15,84 +8,34 @@ import {
   Text,
   useColorScheme,
   View,
+  Dimensions,
+  Platform
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import Pdf from 'react-native-pdf';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+class PdfScreen extends Component {
+	static navigationOptions = {
+		title: 'PDF',
+	};
 
-function Section({children, title}: SectionProps): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+	render() {
+    const source = {uri:'bundle-assets://resume.pdf', cache:true };
+		return <Pdf trustAllCerts={false} source={source} style={styles.pdf}/>;
+	}
 }
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <>
+    {/* <SafeAreaView>
+      <Text>This is some text</Text>
+    </SafeAreaView> */}
+    <PdfScreen/>
+    </>
+
   );
 }
 
@@ -113,6 +56,16 @@ const styles = StyleSheet.create({
   highlight: {
     fontWeight: '700',
   },
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	pdf: {
+		flex: 1,
+		width: Dimensions.get('window').width,
+    // width: 200
+	},
 });
 
 export default App;
